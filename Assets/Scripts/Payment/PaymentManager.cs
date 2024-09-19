f7f2af2b1c175a42d8291a3f377aadb0bd1db281
Fanky21 <11221021@student.itk.ac.id>
2024-09-19 22:14:55 +0800

    adding in app purchase pt1

--- Assets/Scripts/Payment/PaymentManager.cs
+++ Assets/Scripts/Payment/PaymentManager.cs
@@ -2,21 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Purchasing;
+using UnityEngine.Purchasing.Extension;
 
 namespace ITKombat
 {
-    public class PaymentManager : MonoBehaviour
+    public class PaymentManager : MonoBehaviour, IDetailedStoreListener
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
+
+        private static IStoreController storeController;
+        private static IExtensionProvider storeExtensionProvider;
+
+        public static string product_coins_10 = "com.itkombat.coins10";
+        public static string product_coins_20 = "com.itkombat.coins20";
+        public static string product_sub = "com.itkombat.sub";
+
+        public void InitializePurchasing() {
+
+            var purchase_builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance(AppStore.GooglePlay));
+
+            purchase_builder.AddProduct(product_coins_10, ProductType.Consumable);
+            purchase_builder.AddProduct(product_coins_20, ProductType.Consumable);
+            purchase_builder.AddProduct(product_sub, ProductType.Subscription);
+
+            UnityPurchasing.Initialize(this, purchase_builder);
+
         }
 
-        // Update is called once per frame
-        void Update()
+        public void buyCoins(int coins_ammout) {
+            if (coins_ammout == 10) {
+                InitializePurchase(product_coins_10);
+            } else if (coins_ammout == 20) {
+                InitializePurchase(product_coins_20);
+            }
+        }
+
+        public void buySubscribtion() {
+            InitializePurchase(product_sub);
+        }
+
+        public void InitializePurchase(string product_id) {
+            Product product = storeController.products.WithID(product_id);
+
+            storeController.InitiatePurchase(product);
+        }
+
+        public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
         {
-        
+            throw new System.NotImplementedException();
+        }
+
+        public void OnInitializeFailed(InitializationFailureReason error)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnInitializeFailed(InitializationFailureReason error, string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs purchaseEvent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPurchaseFailed(Product product, PurchaseFailureDescription failureDescription)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
