30d249a5b7b27d1d7bf312b6404ca23303f1d6f4
Felix1180 <134295039+Felix1180@users.noreply.github.com>
2024-11-03 17:03:39 +0800

    Slider Hijau, Teleport

--- Assets/Teleporter.cs
+++ Assets/Teleporter.cs
@@ -7,31 +7,11 @@ public class Teleporter : MonoBehaviour
 {
     [SerializeField] private Transform destination;
     public GameObject Objek; 
-    public Button Button; 
     private bool isObjekInside = false;
 
-    private void Start()
-    {
-        if (Button != null)
-        {
-            Button.onClick.AddListener(OnButtonClick); 
-        }
-    }
     public Transform GetDestination()
     {
         return destination;
-    }
-    private void OnButtonClick()
-    {
-        if (isObjekInside) 
-        {
-            Objek.transform.position = destination.position;
-            Debug.Log("Objek ditransfer ke destinasi.");
-        }
-        else
-        {
-            Debug.Log("Objek tidak berada di dalam collider.");
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -40,6 +20,8 @@ public class Teleporter : MonoBehaviour
         {
             isObjekInside = true;
             Debug.Log("Objek berada di dalam collider.");
+            Objek.transform.position = destination.position;
+            Debug.Log("Objek ditransfer ke destinasi.");
         }
     }
 
