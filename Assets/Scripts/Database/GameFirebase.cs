8e267979ba34cf9711c142aee4dc9dc21546317d
princenizroh <princenizroh7@gmail.com>
2025-01-02 18:55:36 +0800

    change gamefirebase for build to android

--- Assets/Scripts/Database/GameFirebase.cs
+++ Assets/Scripts/Database/GameFirebase.cs
@@ -72,7 +72,7 @@ namespace ITKombat
                 if (dependencyStatus == DependencyStatus.Available)
                 {
                     //If they are avalible Initialize Firebase
-                    // InitializeFirebase();
+                    InitializeFirebase();
                 }
                 else
                 {
@@ -85,7 +85,7 @@ namespace ITKombat
         {
             // Initialize the CustomSceneManager
             customSceneManager = new CustomSceneManager();
-            InitializeFirebase();
+            // InitializeFirebase();
 
             string deviceID = SystemInfo.deviceUniqueIdentifier;
             CheckDeviceAuthorization(deviceID);
